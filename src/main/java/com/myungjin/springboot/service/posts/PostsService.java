2c268da6f3ddd83cfac93d79520e3b6c9048c545
myungjin.kim <myunjin.kim@gmail.com>
2020-04-19 15:25:53 +0900

    API 만들기 - 등록

---
+++ src/main/java/com/myungjin/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.myungjin.springboot.service.posts;
+
+import com.myungjin.springboot.domain.posts.PostsRepository;
+import com.myungjin.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
